a18c33da69de2ecb84b13e06afcc53ebdfe65ab3
Ville Säävuori <ville@unessa.net>
2020-12-01 18:13:06 +0200

    fix: updated browserconfig

--- dist/ui.ssr.js
+++ dist/ui.ssr.js
@@ -1,61 +1,8 @@
-'use strict';var vue=require('vue');function _slicedToArray(arr, i) {
-  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
-}
-
-function _arrayWithHoles(arr) {
-  if (Array.isArray(arr)) return arr;
-}
-
-function _iterableToArrayLimit(arr, i) {
-  if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
-  var _arr = [];
-  var _n = true;
-  var _d = false;
-  var _e = undefined;
-
-  try {
-    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
-      _arr.push(_s.value);
-
-      if (i && _arr.length === i) break;
-    }
-  } catch (err) {
-    _d = true;
-    _e = err;
-  } finally {
-    try {
-      if (!_n && _i["return"] != null) _i["return"]();
-    } finally {
-      if (_d) throw _e;
-    }
-  }
-
-  return _arr;
-}
-
-function _unsupportedIterableToArray(o, minLen) {
-  if (!o) return;
-  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
-  var n = Object.prototype.toString.call(o).slice(8, -1);
-  if (n === "Object" && o.constructor) n = o.constructor.name;
-  if (n === "Map" || n === "Set") return Array.from(o);
-  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
-}
-
-function _arrayLikeToArray(arr, len) {
-  if (len == null || len > arr.length) len = arr.length;
-
-  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
-
-  return arr2;
-}
-
-function _nonIterableRest() {
-  throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
-}var script = /*#__PURE__*/vue.defineComponent({
+'use strict';var vue=require('vue');var script = /*#__PURE__*/vue.defineComponent({
   name: 'UiSample',
+
   // vue component name
-  data: function data() {
+  data() {
     return {
       counter: 5,
       initCounter: 5,
@@ -65,71 +12,64 @@ function _nonIterableRest() {
       }
     };
   },
+
   computed: {
-    changedBy: function changedBy() {
-      var _message$amount;
-
-      var _ref = this,
-          message = _ref.message;
-
+    changedBy() {
+      const {
+        message
+      } = this;
       if (!message.action) return 'initialized';
-      return "".concat(message === null || message === void 0 ? void 0 : message.action, " ").concat((_message$amount = message.amount) !== null && _message$amount !== void 0 ? _message$amount : '').trim();
+      return `${message?.action} ${message.amount ?? ''}`.trim();
     }
+
   },
   methods: {
-    increment: function increment(arg) {
-      var amount = typeof arg !== 'number' ? 1 : arg;
+    increment(arg) {
+      const amount = typeof arg !== 'number' ? 1 : arg;
       this.counter += amount;
       this.message.action = 'incremented by';
       this.message.amount = amount;
     },
-    decrement: function decrement(arg) {
-      var amount = typeof arg !== 'number' ? 1 : arg;
+
+    decrement(arg) {
+      const amount = typeof arg !== 'number' ? 1 : arg;
       this.counter -= amount;
       this.message.action = 'decremented by';
       this.message.amount = amount;
     },
-    reset: function reset() {
+
+    reset() {
       this.counter = this.initCounter;
       this.message.action = 'reset';
       this.message.amount = null;
     }
+
   }
-});var _withId = /*#__PURE__*/vue.withScopeId("data-v-8445b280");
+});const _withId = /*#__PURE__*/vue.withScopeId("data-v-8445b280");
 
 vue.pushScopeId("data-v-8445b280");
 
-var _hoisted_1 = {
+const _hoisted_1 = {
   class: "ui-sample"
 };
 
-var _hoisted_2 = /*#__PURE__*/vue.createTextVNode(".");
+const _hoisted_2 = /*#__PURE__*/vue.createTextVNode(".");
 
 vue.popScopeId();
 
-var render = /*#__PURE__*/_withId(function (_ctx, _cache, $props, $setup, $data, $options) {
+const render = /*#__PURE__*/_withId((_ctx, _cache, $props, $setup, $data, $options) => {
   return vue.openBlock(), vue.createBlock("div", _hoisted_1, [vue.createVNode("p", null, [vue.createTextVNode("The counter was " + vue.toDisplayString(_ctx.changedBy) + " to ", 1
   /* TEXT */
   ), vue.createVNode("b", null, vue.toDisplayString(_ctx.counter), 1), _hoisted_2]), vue.createVNode("button", {
-    onClick: _cache[1] || (_cache[1] = function () {
-      return _ctx.increment.apply(_ctx, arguments);
-    })
+    onClick: _cache[1] || (_cache[1] = (...args) => _ctx.increment(...args))
   }, " Click +1 "), vue.createVNode("button", {
-    onClick: _cache[2] || (_cache[2] = function () {
-      return _ctx.decrement.apply(_ctx, arguments);
-    })
+    onClick: _cache[2] || (_cache[2] = (...args) => _ctx.decrement(...args))
   }, " Click -1 "), vue.createVNode("button", {
-    onClick: _cache[3] || (_cache[3] = function ($event) {
-      return _ctx.increment(5);
-    })
+    onClick: _cache[3] || (_cache[3] = $event => _ctx.increment(5))
   }, " Click +5 "), vue.createVNode("button", {
-    onClick: _cache[4] || (_cache[4] = function ($event) {
-      return _ctx.decrement(5);
-    })
+    onClick: _cache[4] || (_cache[4] = $event => _ctx.decrement(5))
   }, " Click -5 "), vue.createVNode("button", {
-    onClick: _cache[5] || (_cache[5] = function () {
-      return _ctx.reset.apply(_ctx, arguments);
-    })
+    onClick: _cache[5] || (_cache[5] = (...args) => _ctx.reset(...args))
   }, " Reset ")]);
 });function styleInject(css, ref) {
   if ( ref === void 0 ) ref = {};
@@ -165,14 +105,16 @@ script.__scopeId = "data-v-8445b280";var script$1 = /*#__PURE__*/vue.defineCompo
       required: false
     }
   },
-  data: function data() {
+
+  data() {
     return {
       isPrimary: false,
       isSecondary: false,
       buttonType: 'button'
     };
   },
-  created: function created() {
+
+  created() {
     if (this.$attrs.primary !== undefined) {
       this.isPrimary = true;
     } else if (this.$attrs.secondary !== undefined) {
@@ -183,28 +125,24 @@ script.__scopeId = "data-v-8445b280";var script$1 = /*#__PURE__*/vue.defineCompo
       this.buttonType = 'submit';
     }
   }
+
 });function render$1(_ctx, _cache, $props, $setup, $data, $options) {
   return vue.openBlock(), vue.createBlock("button", null, "DEMO");
-}script$1.render = render$1;/* eslint-disable import/prefer-default-export */var components=/*#__PURE__*/Object.freeze({__proto__:null,UiSample: script,DemoButton: script$1});var install = function installUi(app) {
-  Object.entries(components).forEach(function (_ref) {
-    var _ref2 = _slicedToArray(_ref, 2),
-        componentName = _ref2[0],
-        component = _ref2[1];
+}script$1.render = render$1;/* eslint-disable import/prefer-default-export */var components=/*#__PURE__*/Object.freeze({__proto__:null,UiSample: script,DemoButton: script$1});// Import vue components
 
+const install = function installUi(app) {
+  Object.entries(components).forEach(([componentName, component]) => {
     app.component(componentName, component);
   });
 }; // Create module definition for Vue.use()
-var components$1=/*#__PURE__*/Object.freeze({__proto__:null,'default': install,UiSample: script,DemoButton: script$1});// only expose one global var, with component exports exposed as properties of
+var components$1=/*#__PURE__*/Object.freeze({__proto__:null,'default': install,UiSample: script,DemoButton: script$1});// iife/cjs usage extends esm default export - so import it all
+// only expose one global var, with component exports exposed as properties of
 // that global var (eg. plugin.component)
 
-Object.entries(components$1).forEach(function (_ref) {
-  var _ref2 = _slicedToArray(_ref, 2),
-      componentName = _ref2[0],
-      component = _ref2[1];
-
+Object.entries(components$1).forEach(([componentName, component]) => {
   if (componentName !== 'default') {
-    var key = componentName;
-    var val = component;
+    const key = componentName;
+    const val = component;
     install[key] = val;
   }
 });module.exports=install;
