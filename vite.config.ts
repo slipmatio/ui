5f8f6aa5cfefb76418cb93b4a734e4eaad013363
Ville Säävuori <ville@unessa.net>
2023-06-24 22:03:01 +0300

    config tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,15 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [
+    vue(),
+    dts({
+      copyDtsFiles: true,
+      outputDir: 'dist',
+      staticImport: true,
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
